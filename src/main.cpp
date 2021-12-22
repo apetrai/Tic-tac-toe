0bfc3cc7fa2fcd9ffaf65ee8f3adb666825809a0
Angelo Petrai <agjelo.petraj10@gmail.com>
2021-12-22 19:34:37 +0000

    Fix Bug, update UI.

--- src/main.cpp
+++ src/main.cpp
@@ -3,72 +3,118 @@
 #include <fstream>
 #include <vector>
 
-#define LINE printf("\t\t|---|---|---|");
+void play(char &A, char &B, bool &turn);
 
 int main() {
-    int choice = 0;
-    char winner = 0;
-    std::string board[9] = { "0", "1", "2", "3", "4", "5" ,"6", "7", "8" };
-    std::string player1;
-    std::string player2;
-    char play1, play2;
-    std::fstream Username_filecheck(".username");
+  bool turn = 0;
+  char A = '\0', B = '\0';
 
-  if(!Username_filecheck) {
-    std::cout << "Username for player 1; ";
-    getline(std::cin, player1);
-    system("clear");
-    std::cout << "Username for player 2; ";
-    getline(std::cin, player2);
-    system("clear");
-        std::cout << "Each player has to select to play either as X or O.\n";
-        std::cout << player1 << " you are; ";
-        std::cin >> play1;
-        system("clear");
-        std::cout << player2 << " you are; ";
-        std::cin >> play2;
-        system("clear");
-    std::ofstream Username(".username");
-    char buffer = toupper(play1);
-    play1 = buffer;
-    buffer = toupper(play2);
-    play2 = buffer;
-    Username << player1 << "\n" << play1 << "\n" << player2 << "\n" << play2;
+  std::cout << "User A: enter your first initial. ";
+  std::cin >> A;
+
+  system("clear");
+
+  std::cout << "User B: enter your first initial. ";
+  std::cin >> B;
+
+  system("clear");
+
+  play(A, B, turn);
+}
+
+void play(char &A, char &B, bool &turn)
+{
+  std::vector<char> PlayerOption = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  char option = '\0';
+ while (true) {
+    std::cout << "\t\t\t\t\t ___________ " << std::endl;
+    std::cout << "\t\t\t\t\t| " << PlayerOption[0] << " | " << PlayerOption[1] << " | " << PlayerOption[2] << " |" << std::endl;
+    std::cout << "\t\t\t\t\t|-----------|" << std::endl;
+    std::cout << "\t\t\t\t\t| " << PlayerOption[3] << " | " << PlayerOption[4] << " | " << PlayerOption[5] << " |" << std::endl;
+    std::cout << "\t\t\t\t\t|-----------|" << std::endl;
+    std::cout << "\t\t\t\t\t| " << PlayerOption[6] << " | " << PlayerOption[7] << " | " << PlayerOption[8] << " |" << std::endl;
+    std::cout << "\t\t\t\t\t|___________|\n\n" << std::endl;
+
+  if( PlayerOption[0] == 'X' && PlayerOption[1] == 'X' && PlayerOption[2] == 'X' || PlayerOption[3] == 'X' && PlayerOption[4] == 'X' && PlayerOption[5] == 'X'
+   || PlayerOption[6] == 'X' && PlayerOption[7] == 'X' && PlayerOption[8] == 'X' || PlayerOption[0] == 'X' && PlayerOption[3] == 'X' && PlayerOption[6] == 'X' //All possibilities of winning for X.
+   || PlayerOption[1] == 'X' && PlayerOption[4] == 'X' && PlayerOption[7] == 'X' || PlayerOption[2] == 'X' && PlayerOption[5] == 'X' && PlayerOption[8] == 'X'
+   || PlayerOption[0] == 'X' && PlayerOption[4] == 'X' && PlayerOption[8] == 'X' || PlayerOption[6] == 'X' && PlayerOption[4] == 'X' && PlayerOption[2] == 'X')
+      exit(EXIT_SUCCESS);
+  else if (PlayerOption[0] == 'O' && PlayerOption[1] == 'O' && PlayerOption[2] == 'O' || PlayerOption[3] == 'O' && PlayerOption[4] == 'O' && PlayerOption[5] == 'O'
+   || PlayerOption[6] == 'O' && PlayerOption[7] == 'O' && PlayerOption[8] == 'O' || PlayerOption[0] == 'O' && PlayerOption[3] == 'O' && PlayerOption[6] == 'O'
+   || PlayerOption[1] == 'O' && PlayerOption[4] == 'O' && PlayerOption[7] == 'O' || PlayerOption[2] == 'O' && PlayerOption[5] == 'O' && PlayerOption[8] == 'O' //All possibilities of winning for O.
+   || PlayerOption[0] == 'O' && PlayerOption[4] == 'O' && PlayerOption[8] == 'O' || PlayerOption[6] == 'O' && PlayerOption[4] == 'O' && PlayerOption[2] == 'O')
+      exit(EXIT_SUCCESS);
+
+  if(turn == false) {
+    std::cout << A << " you are X choose where you want to input 0-8: ";
+    std::cin >> option;
+    switch(option) {
+      case '0':
+          PlayerOption[0] = 'X';
+        break;
+      case '1':
+          PlayerOption[1] = 'X';
+        break;
+      case '2':
+          PlayerOption[2] = 'X';
+        break;
+      case '3':
+          PlayerOption[3] = 'X';
+        break;
+      case '4':
+          PlayerOption[4] = 'X';
+        break;
+      case '5':
+          PlayerOption[5] = 'X';
+        break;
+      case '6':
+          PlayerOption[6] = 'X';
+        break;
+      case '7':
+          PlayerOption[7] = 'X';
+        break;
+      case '8':
+          PlayerOption[8] = 'X';
+        break;
+    }
+    turn = 1;
   }
   else {
-      std::vector<std::string> ContentsofFile(5);
-      std::string buffer;
-      while(Username_filecheck >> buffer) { ContentsofFile.insert(ContentsofFile.begin(), buffer); }
-      player1 = ContentsofFile[1];
-      char* play_buffer = const_cast<char*>(ContentsofFile[0].c_str());
-      play1 = *play_buffer;
-      player2 = ContentsofFile[2];
-      play_buffer = const_cast<char*>(ContentsofFile[3].c_str());
-    play2 = *play_buffer;
-  }
-    while(winner == 0) {
-     system("clear");
-     LINE;
-     std::cout << "\n\t\t| " << board[0] << " | " << board[1] << " | " << board[2] << " |\n";
-     LINE;
-     std::cout << "\n\t\t| " << board[3] << " | " << board[4] << " | " << board[5] << " |\n";
-     LINE;
-     std::cout << "\n\t\t| " << board[6] << " | " << board[7] << " | " << board[8] << " | \n";
-     LINE;
-     std::cout << std::endl;
-     std::cout << player1 << ", your choice; ";
-     std::cin >> choice;
-    if(choice == 0) {board[0] = play1; std::cout << board[0] << std::endl;}
-    else if(choice == 1) {board[1] = play1;}
-    else if(choice == 2) {board[2] = play1;}
-    else if(choice == 3) {board[3] = play1;}
-    else if(choice == 4) {board[4] = play1;} 
-    else if(choice == 5) {board[5] = play1;} 
-    else if(choice == 6) {board[6] = play1;}
-    else if(choice == 7) {board[7] = play1;}
-    else if(choice == 8) {board[8] = play1;}
+    std::cout << B << " you are O choose where you want to input 0-8: ";
+    std::cin >> option;
+    switch(option) {
+    case '0':
+          PlayerOption[0] = 'O';
+        break;
+      case '1':
+          PlayerOption[1] = 'O';
+        break;
+      case '2':
+          PlayerOption[2] = 'O';
+        break;
+      case '3':
+          PlayerOption[3] = 'O';
+        break;
+      case '4':
+          PlayerOption[4] = 'O';
+        break;
+      case '5':
+          PlayerOption[5] = 'O';
+        break;
+      case '6':
+          PlayerOption[6] = 'O';
+        break;
+      case '7':
+          PlayerOption[7] = 'O';
+        break;
+      case '8':
+          PlayerOption[8] = 'O';
+        break;
     }
-
-
+    turn = 0;
+  }
+  system("clear");
+ }
 
 }
